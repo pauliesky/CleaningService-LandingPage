ba4c15be9cb5d36456d11aaaf365f2ba8380c130
Paulo <paulakinleye@gmail.com>
2021-06-11 08:02:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function processForm(){
+
+    fullName = document.getElementById('Name').value 
+    number = document.getElementById('Number').value
+    email = document.getElementById('Email').value
+
+
+    alert('Name='+ fullName+ '\n' +'Number =' + number+ '\n' +'Email=' + email);
+}
+
+  
